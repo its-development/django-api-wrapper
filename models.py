12df387ff0e05feba450bcc20af7dae9391576b9
mzeeb <marc.zeeb@outlook.de>
2021-11-22 20:50:23 +0100

    ENH: Added id as default ApiWrapperModel field

--- models.py
+++ models.py
@@ -1,3 +1,5 @@
+import uuid
+
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 from .manager import ApiWrapperModelManager
@@ -6,11 +8,14 @@ from .manager import ApiWrapperModelManager
 class ApiWrapperModel(models.Model):
     objects = ApiWrapperModelManager
 
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, unique=True)
+
     class Meta:
         abstract = True
 
 
 class ApiWrapperAbstractUser(AbstractUser):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, unique=True)
 
     class Meta:
         abstract = True
