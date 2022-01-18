2069ea78470dc70117562bd59ed456c86b537ce8
mzeeb <marc.zeeb@outlook.de>
2022-01-18 20:07:05 +0100

    FIX: Added default permission checks for ApiWrapperModel

--- models.py
+++ models.py
@@ -13,6 +13,21 @@ class ApiWrapperModel(models.Model):
     class Meta:
         abstract = True
 
+    def check_obj_perm(self, request):
+        return False
+
+    def check_add_perm(self, request):
+        return False
+
+    def check_view_perm(self, request):
+        return False
+
+    def check_change_perm(self, request):
+        return False
+
+    def check_delete_perm(self, request):
+        return False
+
 
 class ApiWrapperAbstractUser(AbstractUser):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, unique=True)
