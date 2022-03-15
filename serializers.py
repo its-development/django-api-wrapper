c0c79d950c2e7ed4fd530f114b182271e667ace3
mzeeb <marc.zeeb@outlook.de>
2022-03-15 21:28:14 +0100

    ENH: ApiWrapperModelSerializer: Added __model__ output

--- serializers.py
+++ serializers.py
@@ -1,5 +1,11 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework import serializers
 
 
-class ApiWrapperModelSerializer(ModelSerializer):
-    pass
+class ApiWrapperModelSerializer(serializers.ModelSerializer):
+    __model__ = serializers.SerializerMethodField("get__model__")
+
+    def get__model__(self, obj):
+        return self.Meta.model.__name__
+
+    class Meta:
+        fields = ["__model__"]
