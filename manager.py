ad3c642ef071382bd8276f467e792682fff80ffa
mzeeb <marc.zeeb@aicentive.com>
2022-07-15 17:08:33 +0200

    ENH: ApiWrapperModelManager: Added annotations to default queryset

--- manager.py
+++ manager.py
@@ -2,4 +2,8 @@ from django.db import models
 
 
 class ApiWrapperModelManager(models.Manager):
-    pass
+    def property_annotate(self, queryset):
+        return queryset
+
+    def get_queryset(self):
+        return self.property_annotate(super().get_queryset())
