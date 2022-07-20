b81fd1b50848958c9a77cd5aa3f2622290e90425
Tim Schelle <12169633+timschelle@users.noreply.github.com>
2022-07-20 14:09:04 +0200

    ENH: separate ApiWrapperUserManager

--- manager.py
+++ manager.py
@@ -1,7 +1,16 @@
+from django.contrib.auth.models import UserManager
 from django.db import models
 
 
 class ApiWrapperModelManager(models.Manager):
+    def property_annotate(self, queryset):
+        return queryset
+
+    def get_queryset(self):
+        return self.property_annotate(super().get_queryset())
+
+
+class ApiWrapperUserManager(UserManager):
     def property_annotate(self, queryset):
         return queryset
 
