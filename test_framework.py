7bd256080e6474712cd4daec00c507300db7b1e0
mzeeb <marc.zeeb@outlook.de>
2024-06-11 10:43:34 +0200

    ENH: test_framework: added compare_fields method

--- test_framework.py
+++ test_framework.py
@@ -1,4 +1,23 @@
 import sys
+from uuid import UUID
+from django.contrib.contenttypes.models import ContentType
+from django.conf import settings
+
+
+def compare_fields(db_item, request_item, assert_method):
+    # Get the content type for the Item model
+    content_type = ContentType.objects.get_for_model(db_item.__class__)
+
+    # Get a list of all related fields for the Item model
+    related_fields = [
+        f.name for f in content_type.model_class()._meta.get_fields() if f.is_relation
+    ]
+
+    for field, value in request_item.items():
+        if field in related_fields:
+            assert_method(getattr(db_item, field + "_id"), UUID(value))
+        else:
+            assert_method(getattr(db_item, field), value)
 
 
 def skip(condition, reason=""):
@@ -19,7 +38,7 @@ def skip_if_in_django_test_command(func):
         if len(sys.argv) >= 2 and sys.argv[1:2] == ["test"]:
             print(
                 f"Skipping {func.__name__} because it is run in a django test command"
-            )
+            ) if settings.DEBUG else None
         else:
             return func(*args, **kwargs)
 
